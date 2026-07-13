97eb83b0de4ab95418061a108c646e0619829446
Naveena <naveenamullapudi777@gmail.com>
2026-07-13 12:51:55 +0530

    Add files via upload

---
+++ IO_Streams/Object_Serialization/EmployeeDemo.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class EmployeeDemo {
+
+    public static void main(String[] args) {
+
+        try {
+            // Creating Employee object
+            SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+            Date dob = sdf.parse("15-08-2002");
+
+            Employee emp = new Employee(
+                    "Alice",
+                    dob,
+                    "IT",
+                    "Software Engineer",
+                    50000.0
+            );
+
+            // Serialization
+            FileOutputStream fos = new FileOutputStream("data");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+            oos.writeObject(emp);
+
+            oos.close();
+            fos.close();
+
+            System.out.println("Employee object serialized successfully.");
+
+            // Deserialization
+            FileInputStream fis = new FileInputStream("data");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            Employee employee = (Employee) ois.readObject();
+
+            ois.close();
+            fis.close();
+
+            // Display Employee details
+            System.out.println("\nEmployee Details");
+            System.out.println("---------------------------");
+            System.out.println("Name        : " + employee.getName());
+            System.out.println("Date of Birth: " + employee.getDateOfBirth());
+            System.out.println("Department  : " + employee.getDepartment());
+            System.out.println("Designation : " + employee.getDesignation());
+            System.out.println("Salary      : " + employee.getSalary());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
